d2561492049805b51a2eb65546a7eeaf07df69fb
M Azhar Nazli <mazhar.nazli@gmail.com>
2024-12-23 17:15:26 +0700

    update config

--- lua/auto.lua
+++ lua/auto.lua
@@ -7,9 +7,11 @@ vim.api.nvim_create_autocmd('VimEnter', {
   end,
 })
 
-vim.api.nvim_create_user_command('CapsReset', function()
-  vim.fn.jobstart 'pkill caps'
-  vim.defer_fn(function()
-    vim.fn.jobstart './caps.exe'
-  end, 100)
-end, {})
+if vim.fn.has 'wsl' == 1 then
+  vim.api.nvim_create_user_command('CapsReset', function()
+    vim.fn.jobstart 'pkill caps'
+    vim.defer_fn(function()
+      vim.fn.jobstart './caps.exe'
+    end, 100)
+  end, {})
+end

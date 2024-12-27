b53c8c4fb3cc8249a8b41eea55bc53ef749ca3d0
M Azhar Nazli <mazhar.nazli@gmail.com>
2024-12-27 21:36:41 +0700

    update config

--- lua/custom/plugins/move.lua
+++ lua/custom/plugins/move.lua
@@ -1,6 +1,15 @@
 return {
   'fedepujol/move.nvim',
-  opts = {
-    --- Config
-  },
+  opts = {},
+  config = function()
+    local opts = { noremap = true, silent = true }
+
+    require('move').setup {}
+    vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+    vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+
+    -- Visual-mode commands
+    vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+    vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
+  end,
 }

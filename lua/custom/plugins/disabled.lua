752fea8a537d92d0ce08cd871e8fda48312eb026
M Azhar Nazli <mazhar.nazli@gmail.com>
2025-02-19 23:26:19 +0700

    update config

--- lua/custom/plugins/disabled.lua
+++ lua/custom/plugins/disabled.lua
@@ -1,7 +1,6 @@
 return {
+  { 'scottmckendry/cyberdream.nvim', enabled = false },
   { 'karb94/neoscroll.nvim', enabled = false },
-  -- { 'folke/trouble.nvim', enabled = false },
   { 'folke/noice.nvim', enabled = false },
-  { 'scottmckendry/cyberdream.nvim', enabled = true },
   { 'zeioth/garbage-day.nvim', enabled = false },
 }

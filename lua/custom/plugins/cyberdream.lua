681703da5cba334738ab15015f7ddc99ee5c81b0
M Azhar Nazli <mazhar.nazli@gmail.com>
2025-06-08 23:05:26 +0700

    update config

--- lua/custom/plugins/cyberdream.lua
+++ lua/custom/plugins/cyberdream.lua
@@ -9,8 +9,5 @@ return {
     italic_comments = false,
     terminal_colors = true,
     variant = 'default', -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
-    highlights = {
-      Visual = { fg = '#f7cb2d', bg = '#696969', italic = true },
-    },
   },
 }

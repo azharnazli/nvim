bb2955f0d02d7eaf47de24cb3e9f6a28ce44d9b6
M Azhar Nazli <mazhar.nazli@gmail.com>
2025-08-09 11:47:23 +0700

    feat: telescope harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -5,10 +5,26 @@ return {
     vim.keymap.set(
       'n',
       '<c-e>',
-      ":lua require('harpoon.ui').toggle_quick_menu()<cr>"
+      ':Telescope harpoon marks<cr>',
+      { silent = true }
     )
-    vim.keymap.set('n', "<c-'>", ":lua require('harpoon.mark').add_file()<cr>")
-    vim.keymap.set('n', '<c-n>', ":lua require('harpoon.ui').nav_next()<cr>")
-    vim.keymap.set('n', '<c-N>', ":lua require('harpoon.ui').nav_prev()<cr>")
+    vim.keymap.set(
+      'n',
+      "<c-'>",
+      ":lua require('harpoon.mark').add_file()<cr>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      'n',
+      '<c-n>',
+      ":lua require('harpoon.ui').nav_next()<cr>",
+      { silent = true }
+    )
+    vim.keymap.set(
+      'n',
+      '<c-N>',
+      ":lua require('harpoon.ui').nav_prev()<cr>",
+      { silent = true }
+    )
   end,
 }

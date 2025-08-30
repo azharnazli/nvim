699394ce29a5afdea347435db634a1762df4dd4c
M Azhar Nazli <mazhar.nazli@gmail.com>
2025-08-31 06:30:53 +0700

    refactor: adjustment diagnostic

--- lua/custom/plugins/lsp-line.lua
+++ lua/custom/plugins/lsp-line.lua
@@ -1,13 +1,21 @@
+-- autocmds: update when cursor moves or holds
 return {
   'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
   config = function()
     require('lsp_lines').setup()
+    vim.diagnostic.config { virtual_text = false }
+    local function show_diagnostics_for_cursor()
+      local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+      local diagnostics = vim.diagnostic.get(0, { lnum = lnum })
+      if #diagnostics > 0 then
+        vim.diagnostic.config { virtual_lines = true }
+      else
+        vim.diagnostic.config { virtual_lines = false }
+      end
+    end
 
-    vim.keymap.set(
-      '',
-      '<Leader>lw',
-      require('lsp_lines').toggle,
-      { desc = 'Toggle lsp_lines' }
-    )
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorMoved' }, {
+      callback = show_diagnostics_for_cursor,
+    })
   end,
 }

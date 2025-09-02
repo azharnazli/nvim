8f3a68ae2dd2d6573fedbef5b85c326aee775e68
M Azhar Nazli <mazhar.nazli@gmail.com>
2025-09-02 15:42:56 +0700

    chore: update config

--- lua/custom/plugins/lsp-line.lua
+++ lua/custom/plugins/lsp-line.lua
@@ -3,11 +3,16 @@ return {
   'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
   config = function()
     require('lsp_lines').setup()
+    local enable = false
+    vim.keymap.set('n', '<leader>lw', function()
+      enable = not enable
+    end)
+
     vim.diagnostic.config { virtual_text = false }
     local function show_diagnostics_for_cursor()
       local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
       local diagnostics = vim.diagnostic.get(0, { lnum = lnum })
-      if #diagnostics > 0 then
+      if #diagnostics > 0 and enable then
         vim.diagnostic.config { virtual_lines = true }
       else
         vim.diagnostic.config { virtual_lines = false }

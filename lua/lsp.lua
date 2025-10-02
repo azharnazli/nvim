db2039b589cc9cdc85ed66ac412060cefcd8b3e6
M Azhar Nazli <mazhar.nazli@gmail.com>
2025-10-02 15:45:13 +0700

    refactor: lsp native

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -8,7 +8,6 @@ vim.lsp.enable {
 
 vim.diagnostic.config {
   virtual_text = true,
-  virtual_lines = true,
   underline = true,
   signs = {
     text = {
@@ -21,28 +20,3 @@ vim.diagnostic.config {
   update_in_insert = false,
   severity_sort = true,
 }
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = {
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx',
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-  },
-  callback = function(args)
-    local buf = args.buf
-    local bufname = vim.api.nvim_buf_get_name(buf)
-
-    -- Check for Node/TS
-    local node_root = vim.fs.find(
-      { 'tsconfig.json', 'jsconfig.json' },
-      { upward = true, path = bufname }
-    )[1]
-    if node_root then
-      vim.lsp.enable 'vtsls'
-      return
-    end
-  end,
-})

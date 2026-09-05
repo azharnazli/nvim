55afec6b4f357794849116cc71f4638ee62d0a16
M Azhar Nazli <mazhar.nazli@gmail.com>
2026-09-05 14:59:46 +0700

    feat: leetcode keybind

--- lua/custom/plugins/leetcode.lua
+++ lua/custom/plugins/leetcode.lua
@@ -2,8 +2,14 @@ return {
   {
     'kawre/leetcode.nvim',
     build = ':TSUpdate html',
+    keys = {
+      { '<leader>Lm', '<cmd>Leet<CR>', desc = 'LeetCode Menu' },
+      { '<leader>Lr', '<cmd>Leet run<CR>', desc = 'LeetCode Run' },
+      { '<leader>Ls', '<cmd>Leet submit<CR>', desc = 'LeetCode Submit' },
+      { '<leader>Li', '<cmd>Leet info<CR>', desc = 'LeetCode Info' },
+    },
     opts = {
-      lang = 'golang',
+      lang = 'javascript',
     },
   },
 }

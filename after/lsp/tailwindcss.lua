77a1be35641d287538ed7fb0f0954a9f40c187ad
M Azhar Nazli <mazhar.nazli@gmail.com>
2026-09-22 17:04:09 +0700

    fix: tailwind lsp start not on tailwind project

--- after/lsp/tailwindcss.lua
+++ after/lsp/tailwindcss.lua
@@ -116,8 +116,6 @@ return {
       'theme/static_src/tailwind.config.mjs',
       'theme/static_src/tailwind.config.ts',
       'theme/static_src/postcss.config.js',
-      -- Fallback for tailwind v4, where tailwind.config.* is not required anymore
-      '.git',
     }
     local fname = vim.api.nvim_buf_get_name(bufnr)
     root_files = util.insert_package_json(root_files, 'tailwindcss', fname)
@@ -127,10 +125,9 @@ return {
       'tailwind',
       fname
     )
-    on_dir(
-      vim.fs.dirname(
-        vim.fs.find(root_files, { path = fname, upward = true })[1]
-      )
-    )
+    local root_marker = vim.fs.find(root_files, { path = fname, upward = true })[1]
+    if root_marker then
+      on_dir(vim.fs.dirname(root_marker))
+    end
   end,
 }
